b1a227e3858ac0642d8017954128c725d898e40f
Gordeev Nikita <nnn.gordeev@gmail.com>
2026-03-14 16:14:59 +0500

    refactor: add asyncHandler middleware to remove try/catch from controllers

--- task-manager/controllers/taskController.js
+++ task-manager/controllers/taskController.js
@@ -1,79 +1,59 @@
 const taskService = require('../services/taskService');
+const asyncHandler = require('../middleware/asyncHandler');
 
 // получение всех задач
-const getTasks = async (req, res, next) => {
-  try {
-    const tasks = await taskService.getAllTasks();
 
-    res.json(tasks);
-  } catch (error) {
-    next(error);
-  }
-};
+const getTasks = asyncHandler(async (req, res) => {
+  const tasks = await taskService.getAllTasks();
+
+  res.json(tasks);
+});
 
 // получить одну задачу по id
-const getTaskById = async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id);
+const getTaskById = asyncHandler(async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const task = await taskService.getTaskById(id);
+  const task = await taskService.getTaskById(id);
 
-    if (!task) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
-
-    res.json(task);
-  } catch (error) {
-    next(error);
+  if (!task) {
+    return res.status(404).json({ message: 'Task not found' });
   }
-};
+
+  res.json(task);
+});
 
 // создание задачи
-const createTask = async (req, res, next) => {
-  try {
-    const newTask = await taskService.createTask(req.body);
+const createTask = asyncHandler(async (req, res) => {
+  const newTask = await taskService.createTask(req.body);
 
-    res.status(201).json(newTask);
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json(newTask);
+});
 
 // удаление задачи
-const deleteTask = async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id);
+const deleteTask = asyncHandler(async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const deletedTask = await taskService.deleteTask(id);
+  const deletedTask = await taskService.deleteTask(id);
 
-    if (!deletedTask) {
-      return res.status(404).json({ message: 'Task not found' });
-    }
-
-    res.json({ message: 'Task deleted successfully' });
-  } catch (error) {
-    next(error);
+  if (!deletedTask) {
+    return res.status(404).json({ message: 'Task not found' });
   }
-};
+
+  res.json({ message: 'Task deleted successfully' });
+});
 
 // обновление задачи
-const updateTask = async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id);
+const updateTask = asyncHandler(async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const updatedTask = await taskService.updateTask(id, req.body);
+  const updatedTask = await taskService.updateTask(id, req.body);
 
-    if (!updatedTask) {
-      const error = new Error('Task not found');
-      error.statusCode = 404;
-      throw error;
-    }
-
-    res.json(updatedTask);
-  } catch (error) {
-    next(error);
+  if (!updatedTask) {
+    return res.status(404).json({ message: 'Task not found' });
   }
-};
+
+  res.json(updatedTask);
+});
 
 // экспортируем функции
 module.exports = {
